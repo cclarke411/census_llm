83d1d9385a5ad01205c0faf5fc4a15689cf617e0
divij-sinha <80167639+divij-sinha@users.noreply.github.com>
2024-02-25 18:43:51 -0600

    add samples for API work

---
+++ src/server/query_api.py
@@ -0,0 +1,32 @@
+### SAMPLES
+api_access_url = "http://api.census.gov/data/2020/dec/dp"
+
+variables = {
+    "DP1_0028C": {
+        "label": "Count!!SEX AND AGE!!Male population!!10 to 14 years",
+        "concept": "PROFILE OF GENERAL POPULATION AND HOUSING CHARACTERISTICS",
+        "predicateType": "int",
+        "group": "DP1",
+        "limit": 0,
+        "attributes": "DP1_0028CA",
+    },
+    "DP1_0029C": {
+        "label": "Count!!SEX AND AGE!!Male population!!15 to 19 years",
+        "concept": "PROFILE OF GENERAL POPULATION AND HOUSING CHARACTERISTICS",
+        "predicateType": "int",
+        "group": "DP1",
+        "limit": 0,
+        "attributes": "DP1_0029CA",
+    },
+    "DP1_0030C": {
+        "label": "Count!!SEX AND AGE!!Male population!!20 to 24 years",
+        "concept": "PROFILE OF GENERAL POPULATION AND HOUSING CHARACTERISTICS",
+        "predicateType": "int",
+        "group": "DP1",
+        "limit": 0,
+        "attributes": "DP1_0030CA",
+    },
+}
+
+geography_fips = {"state": "49", "county": ["011", "013"]}
+###
