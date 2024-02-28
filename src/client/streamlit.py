ef9f0ceb999c231b85bd9006dc1e1efd9b134c62
Abe Burton <abeburton@Abes-MacBook-Pro.local>
2024-02-28 14:54:01 -0600

    modified sidebar for api access and removed chatbot (distracting)

--- src/client/streamlit.py
+++ src/client/streamlit.py
@@ -2,6 +2,7 @@ import sys
 sys.path.append('/Users/abeburton/Desktop/census_llm/src/')
 import server.query_api as q
 from openai import OpenAI
+from openai import APIConnectionError
 from dotenv import load_dotenv
 import streamlit as st
 import os
@@ -24,38 +25,22 @@ st.text(info[1])
 # sidebar with helpful chatbot
 load_dotenv()
 with st.sidebar:
-    
-    user_openai_key = st.text_input("Enter your OpenAI API Key")
-    user_census_key = st.text_input("Enter your Census API Key")
-    
-    st.title("Census Info ChatBot")
-    st.write("ChatBot gives Census info and context")
-    client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+    st.markdown('**Enter your OpenAI API Key**')
+    user_openai_key = st.text_input("For accessing GPT")
+    st.write('*Get your OpenAI key here* :thumbsup: https://help.openai.com/en/articles/4936850-where-do-i-find-my-openai-api-key')
+    st.markdown('**Enter your Census API Key**')
+    user_census_key = st.text_input("For accessing Census Data")
+    st.write('*Get your Census API key here* :thumbsup: https://api.census.gov/data/key_signup.html')
 
-    if "openai_model" not in st.session_state:
-        st.session_state["openai_model"] = "gpt-3.5-turbo"
-
-    if "messages" not in st.session_state:
-        st.session_state.messages = []
-
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
-
-    if prompt := st.chat_input("Ask me about this app!"):
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        with st.chat_message("user"):
-            st.markdown(prompt)
-
-
-        with st.chat_message("assistant"):
-            stream = client.chat.completions.create(
-                model=st.session_state["openai_model"],
-                messages=[
-                    {"role": m["role"], "content": m["content"]}
-                    for m in st.session_state.messages
-                ],
-                stream=True,
-            )
-            response = st.write_stream(stream)
-        st.session_state.messages.append({"role": "assistant", "content": response})
+    if user_openai_key == '':
+        st.error(f'Please enter your OpenAI API Key!', icon="🚨")
+    if user_census_key == '':
+        st.error(f'Please enter your Census API Key!', icon="🚨")
+        
+    st.markdown("**Site Usage:**")
+    st.markdown("""Put any question you want answered using Census/ACS
+                 Data and this site will query the Census and give you data to
+                 answer your question.""")
+    st.markdown("""Examples: How many men are under 45 in
+                 Connectict? How many people commute to work via public transit
+                 in New York versus Los Angeles?""")
