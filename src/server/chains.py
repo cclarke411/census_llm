8ce962c63a9be11b9138750097635ca72e25781c
divij-sinha <80167639+divij-sinha@users.noreply.github.com>
2024-03-02 13:12:37 -0600

    tweak geo

--- src/server/chains.py
+++ src/server/chains.py
@@ -23,10 +23,6 @@ script_path = Path(__file__).resolve()
 script_dir = script_path.parent
 HEAD = "http://api.census.gov/data/"
 
-responses = ["{}"]
-fake_llm = FakeListLLM(responses=responses)
-
-
 def format_docs(docs):
     formatted_str = ""
     for idx, doc in enumerate(docs):
@@ -52,9 +48,11 @@ def get_data(file_path, key, keep):
     return datasets_to_keep
 
 
-def save_docembedding(embeddings_folder_path, datasets):
+def save_docembedding(embeddings_folder_path, datasets, separator="\n\n"):
     data, metadata = zip(*datasets)
-    splitter = CharacterTextSplitter(chunk_size=2750, chunk_overlap=0)
+    splitter = CharacterTextSplitter(
+        chunk_size=2750, chunk_overlap=0, separator=separator
+    )
     xml_docs = splitter.create_documents(data, metadata)
     docembeddings = FAISS.from_documents(
         xml_docs, OpenAIEmbeddings(api_key=os.environ["OPENAIKEY"])
@@ -91,11 +89,14 @@ class SourceChain:
         If you don't know the answer, just say that you don't know, don't try to 
         make up an answer. Only use the provided context to answer.
 
+        Return a json with the keys "geography", "relevant_dataset", and "variables". 
+        the value for the key "geography" should be a list. If no place is mentioned set it to an empty list.
+        the value for the key "relevant_dataset" should be a string. If no dataset is mentioned, set it to NA
+        the value for the key "variables" should be a string. 
+
         This should be in the following format
         Question: [question]
-        Geography: [geography]
-        Variable: [variable category(s)]
-        Dataset: [dataset name]
+        [answer json in specified format goes here]
 
         -----
         
@@ -105,10 +106,7 @@ class SourceChain:
         self.model = ChatOpenAI(
             model="gpt-3.5-turbo", temperature=0, api_key=os.environ["OPENAIKEY"]
         )
-        self.output_parser = RegexParser(
-            regex=r"Geography:(.*?)\nVariable: (.*)\nDataset:(.*)",
-            output_keys=["geography", "variable", "dataset"],
-        )
+        self.output_parser = SimpleJsonOutputParser()
         self.chain = self.prompt | self.model | self.output_parser
 
     def invoke(self, question: str):
@@ -381,25 +379,61 @@ class VarTree:
 class GeographyRAG:
     def __init__(self) -> None:
         self.template = """
-        You are a helpful Census Agent. Only use the provided metadata to answer.
-        You are given a list of geographies, using the attached section, find the relevant list of states and counties as applicable. 
-        If geography specified is not a state or county, find the most relevant state or county.
-        If no geography is specified leave it empty.
-        Return the corresponding FIPS codes only, not the names.
+        As a Census Representative, your objective is to identify the accurate FIPS code 
+        for only the place specified in the query, utilizing only the metadata provided.
 
-        Set Answer equal to a json with the keys "states" and "counties". 
-        "states" should be a list of all the states.
-        "counties" should be a dictionary with the key being the State Level FIPS code, and the value being a list of County level FIPS codes.
+        Here's your guide to handling the queries:
 
-        Provided List:
+        First, determine the state that the queried place is referring to. 
+        In the answer json, set the key "state" equal to the 2-digit FIPS code for the state as requested. 
+        In the answer json, set the key "type" equal to the string "state"
+
+        If the queried place is only referring to a state, stop here and return the resulting json.
+        Remember, if the query exclusively mentions a state, do not use county codes.
+
+        Second, determine if the place in the query specifically refers to a county or county equivalent from the provided data.
+        In the answer json, set the key "county" equal to the 3-digit FIPS code for the county or the county equivalent as requested. 
+        In the answer json, set the key "type" equal to the string "county"
+
+        If the queried place is only referring to a county or county equivalent, stop here and return the resulting json.
+
+        Third, if you have determined that the queried place is not referring to a state or a county or county equivalent, 
+        determine the most appropriate county for this place.
+        In the answer json, set the key "state" equal to the 2-digit FIPS code for the state as requested. 
+        In the answer json, set the key "county" equal to the 3-digit FIPS code for the county or the county equivalent as requested. 
+        In the answer json, set the key "type" equal to the string "other"
+
+        Return the jsom
+
+        - **How to Respond:** Your response should consist of the numeric FIPS codes only, omitting the names. Structure your response as a JSON object with the keys "states", "counties", and "type". 
+       
+        **JSON FORMAT**
+        key: value
+        state: list of state
+        county: dictionary of state:county key:value pairs
+        type: string of type
+        **END OF JSON FORMAT** 
+
+        **Metadata Utilization:** Ensure all information is derived from the given metadata for accuracy.
+
+        **Response Format:** Ensure your answer follows the specified JSON structure for clarity and precision.
+
+        After you think of the answer, cross check whether anything in your answer is not corresponding to the Queried Place. Remove the offending parts.
+        Do this until you are satisfied with your answer.
+        
+        ###QUERIED PLACE###
+        {question}
+        ###END OF QUERY###
+
+        ###METADATA FOR CONTEXT###
         {context}
-
-        Geographies: {question}
+        ###END OF METADATA###
+        
         Answer: """
 
         self.docembeddings = self.get_fips_docembedding()
         self.docretriever = self.docembeddings.as_retriever(
-            search_kwargs={"k": 1},
+            search_kwargs={"k": 3},
         )
         self.prompt = PromptTemplate(
             template=self.template,
@@ -429,16 +463,14 @@ class GeographyRAG:
         datasets_w_meta = []
         for file_name in os.listdir(file_path):
             with open(file_path / file_name, "r") as f:
-                datasets_w_meta.append(
-                    (f.read().replace("\n", "\n\n"), {"file_name": file_name})
-                )
+                datasets_w_meta.append((f.read(), {"file_name": file_name}))
         return datasets_w_meta
 
     def get_fips_docembedding(self):
         fips_path = script_dir / "faiss_index" / "llm_faiss_index_fips"
         if not os.path.exists(fips_path):
             datasets = self.get_fips_data()
-            save_docembedding(fips_path, datasets)
+            save_docembedding(fips_path, datasets, separator="\n")
         docembeddings = FAISS.load_local(
             fips_path, OpenAIEmbeddings(api_key=os.environ["OPENAIKEY"])
         )
